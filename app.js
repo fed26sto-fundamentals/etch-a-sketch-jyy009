643bc33e3b1e6466a6a749b2f237d2b50b394caf
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-09-14 13:10:40 +0200

    Add hover effect for each grid item using JS

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const container = document.querySelector(".container");
 
-for (i = 0; i < 16; i++) {
-  const grid = document.createElement("div");
-  grid.classList.toggle("grid");
-  grid.textContent = "test";
-  container.appendChild(grid);
+for (i = 0; i < 256; i++) {
+  const gridItem = document.createElement("div");
+  gridItem.classList.toggle("grid-item");
+  gridItem.textContent = "test";
+  container.appendChild(gridItem);
 }
+
+// change color of grid item on hover
+const hoverItem = document.querySelectorAll(".grid-item");
+
+hoverItem.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.style.backgroundColor = "#aa83c5";
+    item.style.color = "#ffebcd";
+  });
+});
+
+hoverItem.forEach((item) => {
+  item.addEventListener("mouseout", () => {
+    item.style.backgroundColor = "";
+    item.style.color = "";
+  });
+});
