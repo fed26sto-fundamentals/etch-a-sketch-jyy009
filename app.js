77caddabc8cd5025274f79b19d072a36a6624365
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-09-16 11:21:55 +0200

    Fix grids to fit in container Style the page

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-
 const container = document.querySelector(".container");
-let gridSize = 16
+
+// create button for resetting grid
+const buttonDiv = document.createElement("div");
+buttonDiv.classList.toggle("button-div");
+container.insertAdjacentElement("beforebegin", buttonDiv);
+
+const header = document.createElement("h1");
+header.classList.toggle("header");
+header.textContent = "Etch-a-Sketch";
+buttonDiv.insertAdjacentElement("beforebegin", header);
 
 const button = document.createElement("button");
 button.classList.toggle("button");
-button.textContent = "Redo";
-container.insertAdjacentElement("beforebegin", button);
+button.textContent = "New Grid";
+buttonDiv.appendChild(button);
+
+//create initial grid
+let gridSize = 16;
 
 for (i = 0; i < gridSize * gridSize; i++) {
   const gridItem = document.createElement("div");
   gridItem.classList.toggle("grid-item");
-  gridItem.textContent = "x";
-  // gridItem.style.flexBasis = `calc(100% / ${gridSize})`;
- 
+  // gridItem.textContent = "x";
+  gridItem.style.height = `${600 / gridSize}px`;
+  gridItem.style.width = `${600 / gridSize}px`;
+  gridItem.style.flexBasis = `calc(100% / ${gridSize})`;
+
   container.appendChild(gridItem);
 }
-
 
 // change color of grid item on hover
 const hoverItem = document.querySelectorAll(".grid-item");
@@ -25,12 +37,10 @@ hoverItem.forEach((item) => {
 
   item.addEventListener("mouseover", () => {
     if (!isColored) {
-      item.style.backgroundColor = "#aa83c5";
-      item.style.color = "#ffebcd";
+      item.style.backgroundColor = "#d8acdd";
       isColored = true;
     } else {
       item.style.backgroundColor = "";
-      item.style.color = "";
       isColored = false;
     }
   });
@@ -51,9 +61,9 @@ const createNewGrid = () => {
     }
     newGridItem = document.createElement("div");
     newGridItem.classList.toggle("grid-item");
-    newGridItem.style.height = `${700 / getNewGridSize}px`;
-    newGridItem.style.width = `${700 / getNewGridSize}px`;
-    newGridItem.textContent = "X";
+    newGridItem.style.height = `${600 / getNewGridSize}px`;
+    newGridItem.style.width = `${600 / getNewGridSize}px`;
+    // newGridItem.textContent = "X";
 
     container.appendChild(newGridItem);
   }
